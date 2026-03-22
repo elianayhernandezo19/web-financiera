54154eb83a93d38bee366c92fc0a78d0d155d066
Andrés F. Villarraga A. <andresf.villarragaa@uqvirtual.edu.co>
2026-03-22 00:14:17 -0500

    feat: Add new TopVolume component to the dashboard.

---
+++ src/app/features/dashboard/components/top-volume/top-volume.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-volume',
+  imports: [],
+  templateUrl: './top-volume.html',
+  styleUrl: './top-volume.scss',
+})
+export class TopVolume {}
